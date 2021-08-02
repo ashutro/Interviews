f8759e9306e35e0c1ae74354764f0585df47c0b2
Ashutosh Kumar <unknwnmex@gmail.com>
2021-08-02 21:49:08 +0530

    Adding two number without loop & operator

---
+++ addTwonumber.c
@@ -0,0 +1,13 @@
+//Add two num without using any operator or loop
+
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+    int x = 5, y = 7 ,a,b;
+
+   a = printf("%*c%*c",x,"",y,"");
+
+    printf("\nSum = %d",a);
+
+    return 0;
+}
